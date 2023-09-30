c7ecd3fc18221fe768b743e287ab824b06f6ff60
costaarthur <costaarthur@hotmail.com>
2023-09-30 11:17:16 -0300

    integrate with stripe

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>Eaa</Head>
+      <div>Dalee</div>
+    </>
+  );
+}
